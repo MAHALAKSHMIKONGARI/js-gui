4b3515983928cbbbd621de8869bf7b496365c91d
Mahalakshmi <mahalakshmi.kongari@gmail.com>
2019-09-30 13:21:00 -0500

    commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const add = (x, y) => { return x + y }
+const div = (x, y) => { return x / y }
 
 const validate = async (event) => {
   console.log(`triggered validate on ${event.target.id}`)
@@ -11,29 +11,16 @@ const validate = async (event) => {
   }
 }
 
-const updateWithAdd = async (event) => {
+const updateWithDiv = async (event) => {
   document.querySelector('#result').innerHTML = ''
   if (document.querySelector('#firstNumber').checkValidity() && document.querySelector('#secondNumber').checkValidity()) {
-    const regex = /[^a-zA-Z_]/g
-    const s = document.querySelector('#guest').value.replace(regex, '')
+    
     const i = parseInt(document.querySelector('#firstNumber').value)
     const j = parseInt(document.querySelector('#secondNumber').value)
-    const ans = `${s}, your sum is ${add(i, j)}.`
+    const ans = `The average tasks per developer is ${div(i, j)}`
     document.querySelector('#result').innerHTML = ans
   }
 }
-
-const updateWithJoke = async (event) => {
-  document.querySelector('#result').innerHTML = ''
-  const url = 'https://api.icndb.com/jokes/random?limitTo=[nerdy]'
-  const response = await fetch(url)
-  const obj = await response.json()
-  const joke = obj.value.joke || 'No joke for you.'
-  document.querySelector('#result').innerHTML = joke
-}
-
-// delegate to dynamic elements (e.g. when testing)
-// focusout is like blur, but it bubbles up
 
 document.addEventListener('focusout', event => {
   if ((event.target && event.target.id === 'firstNumber') ||
@@ -43,9 +30,6 @@ document.addEventListener('focusout', event => {
 })
 
 document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'addButton') { updateWithAdd(event) }
+  if (event.target && event.target.id === 'divButton') { updateWithDiv(event) }
 })
 
-document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'getJokeButton') { updateWithJoke(event) }
-})
