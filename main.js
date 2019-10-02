4383f18da0fcd4d71f0b7be12e7909c325cafa63
Mahalakshmi <mahalakshmi.kongari@gmail.com>
2019-10-02 17:53:13 -0500

    commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const div = (x, y) => { return x / y }
+const max = (x, y) => { return Math.max(x, y) }
 
 const validate = async (event) => {
   console.log(`triggered validate on ${event.target.id}`)
@@ -6,18 +6,18 @@ const validate = async (event) => {
   if (isValid) {
     event.target.nextElementSibling.innerHTML = ''
   } else {
-    event.target.nextElementSibling.innerHTML = 'Invalid input'
+    event.target.nextElementSibling.innerHTML = ''
     event.target.focus()
   }
 }
 
-const updateWithDiv = async (event) => {
+const updateWithMax = async (event) => {
   document.querySelector('#result').innerHTML = ''
   if (document.querySelector('#firstNumber').checkValidity() && document.querySelector('#secondNumber').checkValidity()) {
     
     const i = parseInt(document.querySelector('#firstNumber').value)
     const j = parseInt(document.querySelector('#secondNumber').value)
-    const ans = `The average tasks per developer is ${div(i, j)}`
+    const ans = `The Max of ${i} and ${j} is ${Math.max(i, j)}`
     document.querySelector('#result').innerHTML = ans
   }
 }
@@ -30,6 +30,15 @@ document.addEventListener('focusout', event => {
 })
 
 document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'divButton') { updateWithDiv(event) }
+  if (event.target && event.target.id === 'maxButton') { updateWithMax(event) }
 })
 
+
+/*
+function myfunction() {
+  const a = document.getElementById("firststring").value;
+  const b = document.getElementById("secondstring").value;
+  const ans = `The average tasks per developer is ${div(i, j)}`
+  document.getElementById("result").innerHTML = name+" , the biggest String among three is " +biggest;
+}
+*/
